3bb93932d751f321093ee57cd7f528960949c90a
Kimb <mccartykim@zoho.com>
2024-05-03 23:25:09 -0400

    trying to add video acceleration for firefox

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -67,6 +67,20 @@
 
   hardware.bluetooth.enable = true;
 
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      libvdpau-va-gl
+        intel-media-driver
+	intel-vaapi-driver
+	libvdpau-va-gl
+    ];
+  };
+
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
